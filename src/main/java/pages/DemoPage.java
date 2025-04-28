6fa2b5d1309af4cc5b3145d96a4e1d228e8a1308
VarshaLohiya09 <valohiy1@in.ibm.com>
2025-04-28 16:50:31 +0530

    Commit DemoProject to Git Repo

--- src/main/java/pages/DemoPage.java
+++ src/main/java/pages/DemoPage.java
@@ -1,5 +1,6 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -11,14 +12,16 @@ public class DemoPage {
 
     WebDriver driver;
 
+    //Page Elements
+
+    private WebElement getElementById(String id) {
+        return driver.findElement(By.id(id));
+    }
     @FindBy(id = "myTextInput")
     WebElement textInput;
 
     @FindBy(css = "svg rect")
     WebElement svgRect;
-
-    @FindBy(id = "iframeResult")
-    WebElement iframe;
 
     @FindBy(id = "checkBox6")
     private WebElement iframeCheckbox;
@@ -29,18 +32,22 @@ public class DemoPage {
     @FindBy(id = "meterBar")
     WebElement meterBar;
 
+    //Constructor
     public DemoPage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
     }
 
+    //PageMethods
     public void enterTextInput(String text) {
+        WebElement textInput = getElementById("myTextInput");
         textInput.clear();
         textInput.sendKeys(text);
 
     }
 
     public String getTextInputValue() {
+        WebElement textInput = getElementById("myTextInput");
         return textInput.getAttribute("value");
     }
 
@@ -50,24 +57,28 @@ public class DemoPage {
 
     public void toggleIframeCheckbox() {
         driver.switchTo().frame("myFrame3");
+        WebElement iframeCheckbox = getElementById("checkBox6");
         iframeCheckbox.click();
         driver.switchTo().defaultContent();
     }
 
     public void selectDropdownOption(String option) {
-            Select select = new Select(dropDown);
+        WebElement dropdown = getElementById("myDropdown");
+        Select select = new Select(dropDown);
             select.selectByVisibleText(option);
     }
 
     public boolean isIframeCheckboxChecked() {
         driver.switchTo().frame("myFrame3");
+        WebElement iframeCheckbox = getElementById("checkBox6");
         boolean isChecked = iframeCheckbox.isSelected();
         driver.switchTo().defaultContent();
         return isChecked;
     }
 
     public String getMeterValue() {
-            return meterBar.getAttribute("value");
+        WebElement meterBar = getElementById("meterBar");
+        return meterBar.getAttribute("value");
         }
     }
 
